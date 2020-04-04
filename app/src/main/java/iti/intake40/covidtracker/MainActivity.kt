d082e61a216363e6e86ae6ea54594d2e363c0ec1
awatef <awatef.ahmed97@gmail.com>
2020-04-04 09:35:39 +0200

    Initial commit

---
+++ app/src/main/java/iti/intake40/covidtracker/MainActivity.kt
@@ -0,0 +1,20 @@
+package iti.intake40.covidtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //awatef start
+        //awatef end
+        //shorouk start
+        //shorouk end
+    }
+    //awatef start
+    //awatef end
+    //shorouk start
+    //shorouk end
+}
