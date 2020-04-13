b4675089745adbf96f573e3ec0bdbb6a8c7ce829
awatef <awatef.ahmed97@gmail.com>
2020-04-13 19:37:09 +0200

    check for network

--- app/src/main/java/iti/intake40/covidtracker/ui/main/CovidActivity.kt
+++ app/src/main/java/iti/intake40/covidtracker/ui/main/CovidActivity.kt
@@ -2,68 +2,83 @@ package iti.intake40.covidtracker.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import iti.intake40.covidtracker.R
+import iti.intake40.covidtracker.db.model.CovidModel
+import iti.intake40.covidtracker.network.Network
+import kotlinx.android.synthetic.main.activity_covid.*
 
 
 class CovidActivity : AppCompatActivity() {
-    private lateinit var covidViewModel:CovidViewModel
+    private lateinit var covidViewModel: CovidViewModel
+   // lateinit var searchView: SearchView
     // lateinit var covidViewModel:CovidViewModel
-   // var  dataList = ArrayList<CovidModel>()
+   // var dataList = ArrayList<CovidModel>()
     lateinit var recyclerView: RecyclerView
-     //var adapter= CovidAdapter(dataList,this)
+
+   // var adapter = CovidAdapter(dataList , this)
     override fun onCreate(savedInstanceState: Bundle?) {
         //covidViewModel = ViewModelProviders.of(this).get(covidViewModel!!::class.java)
         super.onCreate(savedInstanceState)
-
+        //searchView = findViewById(R.id.search_view)
 
         setContentView(R.layout.activity_covid)
         recyclerView = findViewById(R.id.recycle_view)
 
         covidViewModel = ViewModelProvider(this).get(CovidViewModel::class.java)
-         covidViewModel.getData()
+
+       if (!Network.checkNetworkState(applicationContext)){
+           search_view.setVisibility(View.GONE)
+           Toast.makeText(applicationContext,"افتح النت ي حلوووووووووووف", Toast.LENGTH_SHORT).show()
+
+
+       }else{
+
+
+
+
         covidViewModel.alldata.observe(this, Observer { covids ->
 
             // Update the cached copy of the words in the adapter.
             covids?.let {
-                recyclerView.adapter= CovidAdapter(it, this)
-                recyclerView.layoutManager=LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
-               }
-        })
 
-       // recyclerView.adapter?.notifyDataSetChanged()
+                recyclerView.adapter = CovidAdapter(it as MutableList<CovidModel>, this)
+
+                recyclerView.layoutManager =
+                    LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+                  search(search_view, CovidAdapter(it, this))
+
+
+
+            }
+        })}
+
+
+
+        // recyclerView.adapter?.notifyDataSetChanged()
         //getData()
 
     }
-   /* private fun getData() {
-       //val covidClient: CovidClient
-        val call: Call<ResponseBody> = CovidClient.getClient.getData()
-        call.enqueue(object : Callback<ResponseBody> {
 
-            override fun onResponse(call: Call<ResponseBody>?, response: Response<ResponseBody>?) {
+    private fun search(searchView: SearchView, adapter: CovidAdapter) {
 
-                val obj = JSONObject(response!!.body()!!.string())
-                val arr:JSONArray = obj.getJSONArray("countries_stat")
-                val size = arr.length()
-                for(i in 0 until  size-1){
-                    val detail = arr.getJSONObject(i)
-                    val model = CovidModel(detail.getString("active_cases"),detail.getString("country_name"),
-                        detail.getString("new_cases"),detail.getString("deaths"),
-                        detail.getString("total_recovered"))
-                    dataList.add(model)
-                }
-                //dataList.addAll(response!!.body()!!)
-                recyclerView.adapter?.notifyDataSetChanged()
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(s: String): Boolean {
+                return false
             }
 
-            override fun onFailure(call: Call<ResponseBody>?, t: Throwable?) {
-                println("bbbbbbbbbb")
-
+            override fun onQueryTextChange(s: String): Boolean {
+                adapter.getFilter().filter(s)
+                //println(s)
+                return true
             }
-
         })
-    }*/
+    }
+
 }
