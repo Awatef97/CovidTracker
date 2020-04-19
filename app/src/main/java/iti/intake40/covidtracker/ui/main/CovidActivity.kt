b8fd34e065c0d318a0a89fb896cfa87f0b059e2f
awatef <awatef.ahmed97@gmail.com>
2020-04-19 22:46:37 +0200

    Last Update for setting and splash

--- app/src/main/java/iti/intake40/covidtracker/ui/main/CovidActivity.kt
+++ app/src/main/java/iti/intake40/covidtracker/ui/main/CovidActivity.kt
@@ -4,13 +4,13 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.SearchView
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -18,7 +18,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import iti.intake40.covidtracker.AppPreferences
 import iti.intake40.covidtracker.R
-import iti.intake40.covidtracker.Work.WorkerNotification
 import iti.intake40.covidtracker.db.model.CovidModel
 import iti.intake40.covidtracker.network.Network
 import iti.intake40.covidtracker.ui.main.SettingsActivity.Companion.checkSharedPref
@@ -28,57 +27,72 @@ import kotlinx.android.synthetic.main.activity_covid.*
 class CovidActivity : AppCompatActivity() {
     private lateinit var covidViewModel: CovidViewModel
 
-
     lateinit var recyclerView: RecyclerView
-
+    var flag=true
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         AppPreferences.init(this)
         setContentView(R.layout.activity_covid)
-        checkSharedPref(AppPreferences.isSubscribed)
+
+         checkSharedPref(AppPreferences.isSubscribed)
         recyclerView = findViewById(R.id.recycle_view)
+
         covidViewModel = ViewModelProvider(this).get(CovidViewModel::class.java)
-       covidViewModel.alldata.observe(this, Observer { covids ->
 
-           // Update the cached copy of the words in the adapter.
-           covids?.let {
+        covidViewModel.alldata.observe(this, Observer { covids ->
 
-               recyclerView.adapter = CovidAdapter(it as MutableList<CovidModel>, this)
+            // Update the cached copy of the words in the adapter.
+            covids?.let {
+                search_view.setVisibility(View.VISIBLE)
 
-               recyclerView.layoutManager =
-                   LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-               search(search_view, recyclerView.adapter as CovidAdapter)
+                recyclerView.adapter = CovidAdapter(it as MutableList<CovidModel>, this)
+
+                recyclerView.layoutManager =
+                    LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+                search(search_view, recyclerView.adapter as CovidAdapter)
 
 
-           }
-       })
-       itemsswipetorefresh.setProgressBackgroundColorSchemeColor(ContextCompat.getColor(this, R.color.colorPrimary))
-       itemsswipetorefresh.setColorSchemeColors(Color.WHITE)
-
-       itemsswipetorefresh.setOnRefreshListener {
+            }
+        })
 
 
 
+        if (covidViewModel.alldata.value.isNullOrEmpty() && !Network.checkNetworkState(applicationContext)) {
+            search_view.setVisibility(View.GONE)
+            Toast.makeText(applicationContext, R.string.connectionToast, Toast.LENGTH_SHORT)
+                .show()
 
-           covidViewModel.alldata.observe(this, Observer { covids ->
+            // finish()
+        }
 
-               // Update the cached copy of the words in the adapter.
-               covids?.let {
+        itemsswipetorefresh.setProgressBackgroundColorSchemeColor(ContextCompat.getColor(this, R.color.colorPrimary))
+        itemsswipetorefresh.setColorSchemeColors(Color.WHITE)
 
-                   recyclerView.adapter = CovidAdapter(it as MutableList<CovidModel>, this)
-
-                   recyclerView.layoutManager =
-                       LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-                   search(search_view, recyclerView.adapter as CovidAdapter)
+        itemsswipetorefresh.setOnRefreshListener {
 
 
-               }
-           })
-           itemsswipetorefresh.isRefreshing = false
 
-       }
+            search_view.setVisibility(View.VISIBLE)
+            covidViewModel.getData()
+            covidViewModel.alldata.observe(this, Observer { covids ->
+
+                // Update the cached copy of the words in the adapter.
+                covids?.let {
+
+                    recyclerView.adapter = CovidAdapter(it as MutableList<CovidModel>, this)
+
+                    recyclerView.layoutManager =
+                        LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+                    search(search_view, recyclerView.adapter as CovidAdapter)
+
+
+                }
+            })
+            itemsswipetorefresh.isRefreshing = false
+
+        }
         // recyclerView.adapter?.notifyDataSetChanged()
         //getData()
 
@@ -91,10 +105,19 @@ class CovidActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.getItemId()
+
+
         if (id == R.id.action_one) {
+            // if (Network.checkNetworkState(applicationContext)) {
             val intent = Intent(applicationContext, SettingsActivity::class.java)
             startActivity(intent)
+            /* } else{
+                     Toast.makeText(applicationContext, "check internet connection to subscribe a country to get notification ", Toast.LENGTH_SHORT)
+                         .show()
+                 }*/
+
         }
+
         return true
     }
 
@@ -113,13 +136,5 @@ class CovidActivity : AppCompatActivity() {
             }
         })
     }
-
-//private fun isSubscribed(boolean:Boolean){
-//    when(boolean){
-//        true-> AppPreferences.
-//
-//    }
-
-
 
 }
